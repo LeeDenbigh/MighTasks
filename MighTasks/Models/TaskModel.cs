9a94361f579506b86173dbafe3916956e7b443eb
Lee Denbigh <leedenbigh@googlemail.com>
2023-12-08 16:25:08 +0000

    Code: Created a few models for the tasks.
    - Added a Task Model that contains things like Title, Details, Start Date, etc.
    - Added a Category Model
    - Added a Checklist Model

---
+++ MighTasks/Models/TaskModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MighTasks.Models
+{
+    /// <summary>
+    /// Contains the properties for the task.
+    /// </summary>
+    public class TaskModel
+    {
+        // Property to store the task title.
+        /// <summary>
+        /// Get or set the tasks title.
+        /// </summary>
+        public string TaskTitle { get; set; }
+        // Property to store the task category info
+        /// <summary>
+        /// Get or set the task category info.
+        /// </summary>
+        public CategoryModel TaskCategory { get; set; }
+        // Property to store the task general description.
+        /// <summary>
+        /// Get or set the task details. This is for the task description.
+        /// </summary>
+        public string TaskDetails { get; set; }
+        // Property to store the tasks complete by date.
+        /// <summary>
+        /// Get or set the tasks complete by date.
+        /// </summary>
+        public DateTime TaskCompleteByDateTime { get; set; }
+        // Property to store the tasks started date.
+        /// <summary>
+        /// Store the tasks start date.
+        /// </summary>
+        public DateTime TaskStartDateTime { get; set; }
+        // Boolean property that represents whether the task has been marked as complete.
+        /// <summary>
+        /// Get or set whether the task has been completed.
+        /// </summary>
+        public bool IsComplete { get; set; }
+        // Property to hold the length of time you have worked on a task.
+        /// <summary>
+        /// Get or set the length of time you have worked on this project.
+        /// </summary>
+        public TimeSpan ProjectTimer { get; set; }
+        // List of checklist items for the task.
+        /// <summary>
+        /// Holds all of the check list items for the task.
+        /// </summary>
+        public List<TaskCheckListModel> TaskCheckList { get; set; }
+        /// <summary>
+        /// Get or set the tasks state. (e.g., "InProgress", "Complete").
+        /// </summary>
+        public TaskState TaskState { get; set; }
+    }
+
+    /// <summary>
+    /// Assigns a state to the task.
+    /// </summary>
+    public enum TaskState
+    {
+        /// <summary>
+        /// The task is still in progress.
+        /// </summary>
+        InProgress,
+        /// <summary>
+        /// The task has been marked as completed.
+        /// </summary>
+        Complete,
+        /// <summary>
+        /// The task hasn't been started yet.
+        /// </summary>
+        NotStarted,
+        /// <summary>
+        /// The task is late.
+        /// </summary>
+        Late,
+        /// <summary>
+        /// The task has been archived.
+        /// </summary>
+        Archived,
+        /// <summary>
+        /// The task has been marked as deleted.
+        /// </summary>
+        Deleted
+    }
+}
---
+++ MighTasks/ViewModels/RelayCommandViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MighTasks.ViewModels
+{
+    public class RelayCommandViewModel
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        // Constructor that takes an Action to execute and an optional Func<bool> for canExecute
+        public RelayCommandViewModel(Action execute, Func<bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        // Event that is raised when the ability to execute the command changes
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        // Determines whether the command can be executed
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        // Executes the command
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        // Private field that holds the action to be executed. It's a delegate that takes a parameter of type T and returns nothing.
+        private readonly Action<T> _execute;
+
+        // Private field that holds the predicate to determine if the action can be executed.
+        // It's a delegate that takes a parameter of type T and returns a boolean.
+        private readonly Predicate<T> _canExecute;
+
+        // Constructor for RelayCommand. It takes two parameters: a mandatory action and an optional predicate.
+        // 'execute' is the action to be performed, and 'canExecute' determines if the action can be executed.
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute = null)
+        {
+            // Assigns the action to the _execute field. Throws an ArgumentNullException if 'execute' is null.
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            // Assigns the predicate to the _canExecute field. It's optional and can be null.
+            _canExecute = canExecute;
+        }
+
+        // Method to check if the command can be executed. It takes a parameter and returns a boolean.
+        public bool CanExecute(object parameter)
+        {
+            // Returns true if _canExecute is null (meaning no predicate was provided, so the command can always execute).
+            // Otherwise, it invokes the _canExecute predicate with the casted parameter and returns its result.
+            return _canExecute == null || _canExecute((T)parameter);
+        }
+
+        // Method to execute the command. It takes a parameter and returns nothing.
+        public void Execute(object parameter)
+        {
+            // Invokes the _execute action with the casted parameter.
+            _execute((T)parameter);
+        }
+
+        // Event handler for CanExecuteChanged event. This event is triggered when the execution status of the command changes.
+        public event EventHandler CanExecuteChanged
+        {
+            // Adds a handler to the CommandManager's RequerySuggested event. 
+            // This ensures that CanExecuteChanged is raised whenever the CommandManager suggests requerying the command status.
+            add => CommandManager.RequerySuggested += value;
+            // Removes a handler from the CommandManager's RequerySuggested event.
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+    }
+}
