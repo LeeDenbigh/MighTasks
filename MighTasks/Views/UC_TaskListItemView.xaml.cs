f4bed2061de069f0e52c0828a4d79b288825fb6a
Lee Denbigh <leedenbigh@googlemail.com>
2023-12-08 14:35:31 +0000

    Style: Added a new Task items style and badge colours.
    - Created a user control which will become the task list item.
    - Created the colours, ready for the task badges user control.

---
+++ MighTasks/Views/UC_TaskListItemView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MighTasks.Views
+{
+    /// <summary>
+    /// Interaction logic for UC_TaskListItemView.xaml
+    /// </summary>
+    public partial class UC_TaskListItemView : UserControl
+    {
+        public UC_TaskListItemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
